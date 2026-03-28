2ff89869ebbe649dc02348c3d67dfedf6be1b144
jtannas <jtannas@gmail.com>
2026-03-28 13:25:00 -0600

    adjust for Vite 8

--- src/1-components/ui/card.tsx
+++ src/1-components/ui/card.tsx
@@ -94,10 +94,10 @@ function CardFooter({ className, ...props }: ComponentProps<"div">) {
 
 export {
 	Card,
-	CardHeader,
-	CardFooter,
-	CardTitle,
 	CardAction,
-	CardDescription,
 	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 };
