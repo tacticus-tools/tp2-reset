2f08d7216ebaaa264d0b909224f2405b5b26ff00
jtannas <jtannas@gmail.com>
2026-04-07 14:39:10 -0600

    hide clerkUserId from API responses; prevent role editing

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,8 +1,13 @@
-import { LegacyDataSchema, UserSettingsSchema } from "#common/schemas.js";
 import { zodOutputToConvex } from "convex-helpers/server/zod4";
 import { defineSchema, defineTable } from "convex/server";
 
+import { LegacyDataSchema, UserSettingsSchema } from "#common/schemas.js";
+
 export default defineSchema({
-	legacyData: defineTable(zodOutputToConvex(LegacyDataSchema)).index("by_clerkUserId", ["clerkUserId"]),
-	userSettings: defineTable(zodOutputToConvex(UserSettingsSchema)).index("by_clerkUserId", ["clerkUserId"]),
+	legacyData: defineTable(zodOutputToConvex(LegacyDataSchema)).index("by_clerkUserId", [
+		"clerkUserId",
+	]),
+	userSettings: defineTable(zodOutputToConvex(UserSettingsSchema)).index("by_clerkUserId", [
+		"clerkUserId",
+	]),
 });
